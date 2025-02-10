7a245780622155dfc8eae036c8a6ac06c769f170
joyou73 <joyou94@hotmail.com>
2025-02-10 18:01:05 +0100

    Amélioration du formulaire de connexion : ajout de l'attribut autocomplete pour les champs de saisie et mise à jour des labels pour une meilleure accessibilité.

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -14,26 +14,26 @@
 
 <body class="mt-10">
   <main class="form-signin w-25 m-auto">
-    <form class="d-flex justify-content-center flex-column" onsubmit="handleLogin(event)">
+    <form class="d-flex justify-content-center flex-column" autocomplete="off" onsubmit="handleLogin(event)">
       <div class="d-flex justify-content-center">
         <img class="mb-4" src="assets/images/logo_optitop.png" alt="Optitop logo" width="300" height="128">
       </div>
       <h1 class="h3 mb-3 fw-normal text-center">Connexion</h1>
 
-      <div class="form-floating mb-1">
-        <input type="text" class="form-control" id="login" name="login" placeholder="Identifiant" required>
-        <label for="floatingInput">Identifiant</label>
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="login" name="login" placeholder="Identifiant" autocomplete="off" required>
+        <label for="login">Identifiant</label>
       </div>
 
-      <div class="form-floating">
-        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-        <label for="floatingPassword">Mot de Passe</label>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="password" name="password" placeholder="Mot de Passe" autocomplete="off" required>
+        <label for="password">Mot de Passe</label>
       </div>
 
       <div class="text-start mb-4">
         <a href="forgot-password.html" class="text-muted small">Mot de passe oublié ?</a>
       </div>
-      <button class="btn btn-primary w-100 py-2" type="submit">Se Connecter</button>
+      <button class="btn btn-primary w-100 py-2" type="submit">Se connecter</button>
     </form>
   </main>
   <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
