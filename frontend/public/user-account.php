99c4fb89611ab587fdbdb535dbd676a302bb2fb7
joyou73 <joyou94@hotmail.com>
2025-02-23 12:47:32 +0100

    Ajout de la page "Mon compte" et mise à jour du lien dans l'en-tête pour y accéder.

---
+++ frontend/public/user-account.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon compte</title>
+    <meta name="author" content="Joy Huré">
+    <link rel="icon" href="assets/images/favicon.png">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/styles/custom.css">
+</head>
+
+<body>
+    <?php
+    $pageTitle = "Mon compte";
+    require_once 'components/header.html';
+    require_once 'components/navbar.php';
+    ?>
+    <main>
+
+    </main>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/header.js"></script>
+    <script src="assets/js/navbar.js"></script>
+</body>
